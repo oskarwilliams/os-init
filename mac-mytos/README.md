618c58756fd4418d01bfc54d9a07efdea2d0fb1f
Oskar Williams <oskarwilliams378@proton.me>
2024-02-26 22:37:21 +0000

    feat: Split out mytos init script

---
+++ mac-mytos/README.md
@@ -0,0 +1,7 @@
+# Setup mac
+
+1. Run `./init.sh` in this directory
+2. There may be a few manual prompts where you will have to enter a password or say yes
+3. Open iterm and install the Meslos fonts
+4. Open iterm -> `iTerm2` -> `Settings...` -> `General` -> `Preferences` and check the `Load preferences from a custom folder or URL` box, specifying the custom folder to be the `iterm` folder in this directory
+5. You may need to manually set the iterm font at `iTerm2` -> `Settings...` -> `Profiles` -> `Default` -> `Text` -> `Font` dropdown
---
+++ mac-mytos/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Install and update xcode and other recommended software
+xcode-select --install
+softwareupdate -i -r
+
+# Install Oh My Zsh if not installed
+if [ ! -d  $HOME/.oh-my-zsh ]
+then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install Powerlevel10K if not installed
+if [ ! -d  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]
+then
+    echo "Installing Powerlevel10K"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+# Install brew if not installed
+if ! command -v brew &> /dev/null
+then
+    echo "Installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Install nvm if not installed
+if ! command -v nvm &> /dev/null
+then
+    echo "Installing nvm"
+    sh -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.6/install.sh)"
+fi
+
+# Install Brew casks and formulas
+brew bundle --file=Brewfile
+
+# Copy over initial config
+cp .zshrc.initial ~/.zshrc
+cp .p10k.zsh.initial ~/.p10k.zsh
+cp functions.zsh ~/.oh-my-zsh/custom/functions.zsh
